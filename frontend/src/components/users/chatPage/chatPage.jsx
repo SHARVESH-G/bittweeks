118d22f9bbb1560e7d42e0140ddbc3dd25614c64
SHARVESH-G <sharveshg2405@gmail.com>
2025-07-09 21:21:44 +0530

    Chats Done

--- frontend/src/components/users/chatPage/chatPage.jsx
+++ frontend/src/components/users/chatPage/chatPage.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState, useRef } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import io from "socket.io-client";
 import axios from "axios";
 import { IoSend } from "react-icons/io5";
 import { Avatar } from "@mui/material";
+import { IoMdArrowRoundBack } from "react-icons/io";
+
+
+
 
 const socket = io("http://localhost:3000");
 
@@ -69,16 +73,20 @@ const ChatPage = ({ currentUserId }) => {
   return (
     <div className="relative w-full sm:h-[90vh] bg-[var(--colorPrimary)]/20 overflow-hidden h-[82vh] by-4">
       <div className="absolute top-0 left-0 w-full z-10 p-4 bg-white shadow flex items-center gap-3 border-b">
-        <Avatar
-          src={recipientInfo?.profilePic || ""}
-          sx={{ width: 40, height: 40, fontSize: 16 }}
-        >
-          {!recipientInfo?.profilePic && recipientInfo?.name?.[0]?.toUpperCase()}
-        </Avatar>
-        <h2 className="text-lg font-semibold text-gray-800">
-          {recipientInfo?.name || "Chat"}
-        </h2>
+        <Link to='/users' className="px-2 py-1 rounded flex items-center gap-2 text-[var(--colorPrimary)] border-1 border-[var(--colorPrimary)] hover:text-white hover:bg-[var(--colorPrimary)]"><IoMdArrowRoundBack/>Back</Link>
+        <div className="flex items-center gap-2">
+          <Avatar
+            src={recipientInfo?.profilePic || ""}
+            sx={{ width: 40, height: 40, fontSize: 16 }}
+            >
+            {!recipientInfo?.profilePic && recipientInfo?.name?.[0]?.toUpperCase()}
+          </Avatar>
+          <h2 className="text-lg font-semibold text-gray-800">
+            {recipientInfo?.name || "Chat"}
+          </h2>
+        </div>
       </div>
+
       <div
         className="absolute w-full z-10 px-4 py-2 bg-white shadow-inner border-t border-gray-200"
         style={{
--- frontend/src/components/users/messageSection/messageSection.jsx
+++ frontend/src/components/users/messageSection/messageSection.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Avatar } from '@mui/material'
+
 
 const MessageSection = ({ currentUserId }) => {
   const [users, setUsers] = useState([]);
@@ -33,11 +35,14 @@ const MessageSection = ({ currentUserId }) => {
         <div
           key={user._id}
           onClick={() => openChat(user._id)}
-          className="flex items-center justify-between px-4 py-3 border-b hover:bg-gray-100 cursor-pointer"
+          className="flex items-center justify-between px-4 py-3 border-2 border-gray-300 hover:bg-gray-100 cursor-pointer my-2 rounded-2xl"
         >
-          <div>
-            <p className="font-medium text-gray-800">{user.name}</p>
-            <p className="text-sm text-gray-600">{lastMessage}</p>
+          <div className="flex gap-4">
+            <Avatar src={user?.profilePic}>{user.name.slice(0,1)}</Avatar>
+            <div>
+              <p className="font-medium text-gray-800">{user.name}</p>
+              <p className="text-sm text-gray-600">{lastMessage}</p>
+            </div>
           </div>
           {unreadCount > 0 && (
             <span className="bg-red-500 text-white text-xs font-semibold px-2 py-0.5 rounded-full">
